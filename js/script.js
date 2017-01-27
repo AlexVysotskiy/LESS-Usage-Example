122b7a34db20d187ce9b559c471913da9cf36b15
Vysotskiy Alexander <vysotskij.av@gmail.com>
2017-01-27 16:41:22 +0300

    finished

---
+++ js/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+
+    var map = $('#map');
+    if (map.length) {
+        
+        var myMap,
+            myPlacemark;
+
+        ymaps.ready(function () {
+
+            myMap = new ymaps.Map("map", {
+                center: [51.539825, 46.025651],
+                zoom: 15
+            });
+
+            myPlacemark = new ymaps.Placemark([51.539825, 46.025651], {
+                hintContent: 'PlaceMerk',
+                balloonContent: ''
+            }, {
+                iconLayout: 'default#image',
+                iconImageHref: 'img/place-mark.png',
+                iconImageSize: [39, 52]
+            });
+
+            myMap.geoObjects.add(myPlacemark);
+
+        });
+    }
+
+});
